96699be9284b4f53b327121a426162f87b4d7c86
sean-maclane <43688037+sean-maclane@users.noreply.github.com>
2020-03-24 13:59:45 -0700

    fix test install

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=["flask", "marshmallow"],
+    install_requires=["flask", "marshmallow", "wheel"],
     extras_require={"test": ["pytest", "coverage"]},
 )
