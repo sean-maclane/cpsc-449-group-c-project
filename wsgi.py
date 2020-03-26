510f42289bbeb8d674b7ef4fb7ca6b6ef9ad35b7
Saurabh Mishra <m.saura8h@gmail.com>
2020-03-25 22:19:26 -0700

    updated application entrypoint

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 # Application endpoint for gunicorn
-from main import app
+from project1 import create_app
+
+app = create_app()
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=2015)
-
