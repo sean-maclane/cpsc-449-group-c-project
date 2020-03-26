f1e7f8117972a1c5e672e7539c65ef3e9f66de7e
Saurabh Mishra <m.saura8h@gmail.com>
2020-03-25 20:08:29 -0700

    dev and test packages clubbed together

--- setup.py
+++ setup.py
@@ -16,6 +16,5 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=["flask", "marshmallow", "wheel"],
-    extras_require={"test": ["pytest", "coverage"]},
+    install_requires=["flask", "marshmallow", "wheel", "pytest", "coverage"]
 )
