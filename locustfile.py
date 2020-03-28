cfef9983d68ab85e3913e88691febfcacc0793e5
Saurabh Mishra <m.saura8h@gmail.com>
2020-03-28 16:23:52 -0700

    load testing cases added for VOTES and POSTS

--- locustfile.py
+++ locustfile.py
@@ -18,6 +18,9 @@ class UserBehavior(TaskSet):
         self.username = next(username_generator)
         self.email = next(email_generator)
         self.password = next(password_generator)
+        self.post_title = next(post_title_generator)
+        self.post_body = next(post_text_generator)
+        self.community = next(community_generator)
 
         self.create_account()
     
@@ -42,34 +45,52 @@ class UserBehavior(TaskSet):
         data = {"username": self.username , "password": self.password, "email": self.email}
         self.client.post(url, data)
 
-    '''
-    @task
-    def app_homepage(self):
-        response = self.client.get('/')
-        print("Response status code:", response.status_code)
-        print("Response content:", response.text)
-    
-	To be verifies with test cases GET, POST, etc.
     @task
     def upvote(self):
-        self.client.get('/votes/upvote')
+        upvote_url = '/votes/upvote'
+        upvote_data = {'username': self.username, 'password': self.password}
+        self.client.post(upvote_url, upvote_data)
         
     @task
     def downvote(self):
-        self.client.get('/votes/downvote')
+        downvote_url = '/votes/downvote'
+        downvote_data = {'username': self.username, 'password': self.password}
+        self.client.post(downvote_url, downvote_data)
+
+    @task(1)
+    def retrieve_post(self):
+        retrieve_url = '/posts/retrieve'
+        retrieve_data1 = {'title': '', 'community': ''}                             # case 1 from test_posts.py
+        retrieve_data2 = {'title': self.post_title, 'community': ''}                # case 2 from test_posts.py
+        retrieve_data3 = {'title': '', 'community': self.community}                 # case 3 from test_posts.py
+        retrieve_data4 = {'title': self.post_title, 'community': self.community}    # case 4 from test_posts.py
+        self.client.post(retrieve_url, retrieve_data1)
+        self.client.post(retrieve_url, retrieve_data2)
+        self.client.post(retrieve_url, retrieve_data3)
+        self.client.post(retrieve_url, retrieve_data4)
 
     @task
     def create_post(self):
-        self.client.get('/posts/create')
+        createpost_url = '/posts/create'
+        createpost_data = {
+            'title': self.post_title,
+            'community': self.community,
+            'text': self.post_body,
+            'username': self.username,
+            'url': 'posts_create.com'
+        }
+        self.client.post(createpost_url, createpost_data)
         
     @task
     def delete_post(self):
-        self.client.get('/posts/delete')
-        
-    @task
-    def retrieve_post(self):
-        self.client.get('/posts/retrieve')
-    '''
+        deletepost_url = '/posts/delete'
+        deletepost_data = {
+            'title': self.post_title,
+            'username': self.username,
+            'password': self.password
+        }
+        self.client.post(deletepost_url, deletepost_data)
+    
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
