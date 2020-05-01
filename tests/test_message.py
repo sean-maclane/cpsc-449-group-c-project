3778090937b7d186b1d3c0cfff306ecdda06f198
prepha <prepha15@csu.fullerton.edu>
2020-05-01 16:25:19 -0700

    still in progress 90% done

--- tests/test_message.py
+++ tests/test_message.py
@@ -11,35 +11,39 @@ from project1.db import get_db
 def test_message_send(client, app):
     # Add existing message for error testing
     with app.app_context():
-        get_db().execute('INSERT INTO users (username, email, password, karma) VALUES (?, ?, ?, ?);',
-                         ("existing_accounts_create", "existing_accounts@create.com", "existing_accounts_create", 0))
+        get_db().execute('INSERT INTO messages (userfrom, userto, messagecontent,flag) VALUES (?, ?, ?, ?);',
+                         ("bob", "mary", "hello", 0))
         get_db().commit()
 
     url = "/message/send"
 
     # test a valid POST request
-    valid_data = {"username": "accounts_create",
-                  "email": "accounts@create.com", "password": "accounts_create"}
+    valid_data = {"userfrom": "bob",
+                  "userto": "mary", "messagecontent": "hello", "flag": 0}
     assert client.post(url, data=valid_data).status_code == 201
 
     # test that the user was inserted into the database
     with app.app_context():
         assert (
-            get_db().execute("SELECT * FROM users WHERE username = 'accounts_create'").fetchone()
+            get_db().execute("SELECT * FROM messages WHERE userfrom = 'bob'").fetchone()
             is not None
         )
 
 
 @pytest.mark.parametrize(
-    ("username", "email", "password", "message", "http_status_code"),
+    ("userfrom", "userto", "messagecontent",
+     "flag", "message", "http_status_code"),
     (
-        ("", "", "", b"Error in creating your account", 404),
-        ("other2", "bademail.com", "other2", b"Not proper Email", 404),
+        ("", "mary", "hello", 0, b"No user from", 404),
+        ("bob", "", "hello", 0, b"No user to", 404),
+        ("bob", "mary", "", 0, b"No message", 404),
+
     ),
 )
-def test_message_send_validate(client, username, email, password, message, http_status_code):
+def test_message_send_validate(client, userfrom, userto, messagecontent, flag, message, http_status_code):
     url = "/message/send"
-    bad_data = {"username": username, "email": email, "password": password}
+    bad_data = {"userfrom": userfrom, "userto": userto,
+                "messagecontent": messagecontent, "flag": flag}
 
     response = client.post(url, data=bad_data)
 
@@ -51,29 +55,29 @@ def test_message_send_validate(client, username, email, password, message, http_
 def test_message_delete(client, app):
     # Add user for delete testing
     with app.app_context():
-        get_db().execute('INSERT INTO users (username, email, password, karma) VALUES (?, ?, ?, ?);',
+        get_db().execute('INSERT INTO messages (userfrom, userto, messagecontent, flag) VALUES (?, ?, ?, ?);',
                          ("accounts_updateEmail", "accounts@updateEmail.com", "accounts_updateEmail", 0))
-        get_db().execute('INSERT INTO users (username, email, password, karma) VALUES (?, ?, ?, ?);',
+        get_db().execute('INSERT INTO messages (userfrom, userto, messagecontent, flag) VALUES (?, ?, ?, ?);',
                          ("accounts_updateEmail_2", "accounts_2@updateEmail_2.com", "accounts_updateEmail_2", 0))
         get_db().commit()
 
     url = "/message/delete"
 
     # test a valid POST request
-    valid_data = {"username": "accounts_updateEmail",
-                  "email": "new_accounts@updateEmail.com", "password": "accounts_updateEmail"}
+    valid_data = {"userfrom": "accounts_updateEmail",
+                  "messagecontent": "new_accounts@updateEmail.com"}
     assert client.post(url, data=valid_data).status_code == 201
 
     # test that the new email was inserted into the database
     with app.app_context():
         assert (
-            get_db().execute("SELECT * FROM users WHERE username = 'accounts_updateEmail' and email = 'new_accounts@updateEmail.com'").fetchone()
+            get_db().execute("SELECT * FROM messages WHERE userfrom = 'accounts_updateEmail' and email = 'new_accounts@updateEmail.com'").fetchone()
             is not None
         )
 
 
 @pytest.mark.parametrize(
-    ("username", "email", "password", "message", "http_status_code"),
+    ("userfrom", "messagecontent", "message", "http_status_code"),
     (
         ("", "", "", b"Provided information", 404),
         ("nonexistant_username", "new@new.com",
@@ -82,9 +86,9 @@ def test_message_delete(client, app):
          b"Enter a new email for account", 404),
     ),
 )
-def test_message_delete_validate(client, username, email, password, message, http_status_code):
+def test_message_delete_validate(client, userfrom, messagecontent, message, http_status_code):
     url = "/message/delete"
-    bad_data = {"username": username, "email": email, "password": password}
+    bad_data = {"userfrom": userfrom, "messagecontent": messagecontent}
 
     response = client.post(url, data=bad_data)
 
@@ -115,13 +119,13 @@ def test_message_favorite(client, app):
 
 
 @pytest.mark.parametrize(
-    ("username", "password", "message", "http_status_code"),
+    ("username", "message", "message", "http_status_code"),
     (
         ("", "", b"Provide Information", 404),
         ("nonexistant_username", "wrong", b"No account to delete", 404),
     ),
 )
-def test_message_favorite_validate(client, username, password, message, http_status_code):
+def test_message_favorite_validate(client, username, messagecontent, message, http_status_code):
     url = "/message/favorite"
     bad_data = {"username": username, "password": password}
 
