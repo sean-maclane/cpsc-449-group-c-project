b447df33381ddbd41235de3725df29af839348b8
prepha <prepha15@csu.fullerton.edu>
2020-05-01 21:42:11 -0700

    95% done

--- project1/message.py
+++ project1/message.py
@@ -13,37 +13,25 @@ bp = Blueprint("message", __name__, url_prefix='/message')
 def sendMessage():
     db = get_db()
 
-    _id = request.form['id']
     _userfrom = request.form['userfrom']
     _userto = request.form['userto']
-    _ts = request.form['ts']
     _message = request.form['messagecontent']
     _flag = request.form['flag']
 
-    valid_userfrom = db.execute(
-        'SELECT userName FROM users WHERE username = ? ', (_userfrom)).fetchone()
-    valid_userto = db.execute(
-        'SELECT userName FROM users WHERE username = ?', (_userto)).fetchone()
-    valid_message = db.execute(
-        'SELECT messagecontent FROM users WHERE messagecontent = ?', (_message)).fetchone()
+    if _userfrom == "":
 
-    if valid_userfrom is None:
-        # error case 1, no user in database to send messages to
-        return Response(json.dumps({"message": "users don't exist"}), status=404, content_type="application/json")
+        return Response(json.dumps({"message": "No user from"}), status=404, content_type="application/json")
 
-    if valid_userfrom is not None and valid_userto is None:
-        # error case 2
-        return Response(json.dumps({"message": "receiver account doesn't exist"}), status=404, content_type="application/json")
+    if _userto == "":
 
-    if valid_userfrom is None and valid_userto is not None:
-        return Response(json.dumps({"message": "sending account doesn't exist"}), status=404, content_type="application/json")
+        return Response(json.dumps({"message": "No user to"}), status=404, content_type="application/json")
 
-    if valid_message is None:
-        return Response(json.dumps({"message": "provide a message"}), status=404, content_type="application/json")
+    if _message == "":
+        return Response(json.dumps({"message": "No message"}), status=404, content_type="application/json")
 
     else:
         db.execute(
-            'INSERT INTO messages (userfrom,userto,ts,messagecontent,flag) VALUES (?,?,?,?,?)', (_userfrom, _userto, _ts, _message, _flag))
+            'INSERT INTO messages (userfrom,userto,messagecontent,flag) VALUES (?,?,?,?)', (_userfrom, _userto, _message, _flag))
         db.commit()
     return Response(status=201)
 
@@ -55,13 +43,9 @@ def deleteMessage():
     _userfrom = request.form['userfrom']
     _message = request.form['messagecontent']
 
-    if _userfrom is None:
+    if _userfrom == "":
         # error case 1
         return Response(json.dumps({"message": "user doesn't exist"}), status=404, content_type="application/json")
-
-    if _message is None:
-        # error case 2
-        return Response(json.dumps({"message": "message doesn't exist"}), status=404, content_type="application/json")
 
     if _message == "":
         return Response(json.dumps({"message": "no message provided"}), status=404, content_type="application/json")
@@ -69,16 +53,18 @@ def deleteMessage():
         return Response(status=201)
 
 
-@bp.route('/favorite', methods['GET'])
+@bp.route('/flag', methods['GET,POST'])
 def favoriteMessage():
 
     _message = request.form['messagecontent']
     _flag = request.form['flag']
 
-    if _message is None:
+    if _message == "":
         return Response(json.dumps({"message": "message can't be found"}), status=404, content_type="application/json")
-
-    return Response(status=201)
+    if _flag == 0:
+        return Response(json.dumps({"message": "message isn't flagged"}), status=404, content_type="application/json")
+    else:
+        return Response(status=201)
 
 
 # WSGI entrypoint for VOTES
